e53ab1a2071a9ac8ed85ef0989ee29b392c7e9ab
liujc <liujuncheng@xizhengtech.com>
2024-01-02 18:09:47 +0800

    append ConfigurableApplicationContext comment

--- framework/src/main/java/org/example/n01_basic/App.java
+++ framework/src/main/java/org/example/n01_basic/App.java
@@ -2,22 +2,18 @@ package org.example.n01_basic;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.util.Objects;
-
 public class App {
 
     public static void main(String[] args) {
         System.setProperty("basicPath","n01_basic/basic.xml");
-        ClassPathXmlApplicationContext context = null;
-        try {
-            context = new ClassPathXmlApplicationContext("classpath:${basicPath}");
+
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:${basicPath}")){
+
             final SimpleBean bean = context.getBean(SimpleBean.class);
 
             bean.send();
-        } finally {
-            if (Objects.nonNull(context)) {
-                context.close();
-            }
+        }finally {
+            System.out.println("end...");
         }
     }
 
